bc4f05428efd53b4026fb7e986cbf6dd1668c324
xu.xiaojie@trans-cosmos.com.cn <xuxiaojie@jntci.com.cn>
2024-05-08 14:37:52 +0800

    余計なものを削除する

--- sbApi/src/main/java/com/web/app/service/ParticipatedStatusChangeService.java
+++
@@ -1,8 +0,0 @@
-package com.web.app.service;
-
-import com.web.app.domain.ParticipatedStatusChangeResultInfo;
-
-public interface ParticipatedStatusChangeService {
-    ParticipatedStatusChangeResultInfo ParticipatedStatusChangeInfoSearch(String caseId);
-
-}
