be9dc5f859f7f54de84befe79cf1f2bb8a5da650
jiawenzhi <jia.wenzhi@trans-cosmos.com.cn>
2024-05-09 17:30:56 +0900

    17:30

--- sbApi/src/main/java/com/web/app/mapper/UpdNegotiatAgreeMapper.java
+++ sbApi/src/main/java/com/web/app/mapper/UpdNegotiatAgreeMapper.java
@@ -1,13 +1,15 @@
 package com.web.app.mapper;
+
 import org.apache.ibatis.annotations.Mapper;
 import com.web.app.domain.ReconciliationUser;
 import com.web.app.domain.ActionHistories;
 import com.web.app.domain.Entity.Cases;
 
 /**
- * 和解案合意更新API 
- * 「アクロン履歴」新規登録
+ * 和解案合意更新API
+ * 和解案合意更新API
  * メール送信
+ * 
  * @author DUC jiawenzhi
  * @since 2024/05/09
  * @version 1.0
@@ -15,12 +17,15 @@ import com.web.app.domain.Entity.Cases;
 
 @Mapper
 public interface UpdNegotiatAgreeMapper {
-
+    // 和解案合意更新API
     int reconciliationUpdate(ReconciliationUser reconciliationUser);
 
+    // 和解案合意更新API
     int ActionHistoriesInsert(ActionHistories ActionHistories);
 
+    // メール送信
     ActionHistories UserSearch(ActionHistories ActionHistories);
-    
+
+    // casesテーブルのCaseTitleを検索し、メール送信に設定する
     String CaseTitleSearch(Cases cases);
 }
--- sbApi/src/main/java/com/web/app/service/impl/UpdNegotiatAgreeImpl.java
+++ sbApi/src/main/java/com/web/app/service/impl/UpdNegotiatAgreeImpl.java
@@ -93,7 +93,7 @@ public class UpdNegotiatAgreeImpl implements UpdNegotiatAgreeService {
         }
         return 1;
     }
-
+    
     /**
      * メール送信
      * 
