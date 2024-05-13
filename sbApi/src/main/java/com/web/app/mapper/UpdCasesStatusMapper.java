312fa39bd041072600bc00fcd814c6855e639b3e
zhuxiaofang2024 <zhu.xiaofang@trans-cosmos.com.cn>
2024-05-13 18:08:42 +0900

    up by 24053-2

--- sbApi/src/main/java/com/web/app/mapper/UpdCasesStatusMapper.java
+++ sbApi/src/main/java/com/web/app/mapper/UpdCasesStatusMapper.java
@@ -2,12 +2,12 @@ package com.web.app.mapper;
 
 import org.apache.ibatis.annotations.Mapper;
 
-import com.web.app.domain.ParticipatedStatusChangeSelectInfo;
+import com.web.app.domain.Entity.Cases;
 
 @Mapper
 public interface UpdCasesStatusMapper {
     // 参加表明対象ケースの状態の判定
-    ParticipatedStatusChangeSelectInfo participatedStatusChangeInfoSearch(String caseId);
+    Cases participatedStatusChangeInfoSearch(String caseId);
 
     // ケースの状態の更新
     int caseStatusChangeUpdate(String caseId);
