58be97408fd91787756ccfaeea61824aaaa92d4d
xinzhaoyan <xin.zhaoyan@trans-cosmos.com.cn>
2024-05-15 13:21:33 +0800

    update1144

--- sbApi/src/main/java/com/web/app/controller/UserLoginController.java
+++ sbApi/src/main/java/com/web/app/controller/UserLoginController.java
@@ -2,7 +2,10 @@
 package com.web.app.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
 import com.web.app.domain.userLogin.GetPreUserData;
 import com.web.app.domain.Response;
 import com.web.app.service.UserLoginService;
