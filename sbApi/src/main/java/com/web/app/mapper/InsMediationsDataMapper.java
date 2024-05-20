5a85557744f911fcbe2e47be717f8efeada75caf
jiawenzhi <jia.wenzhi@trans-cosmos.com.cn>
2024-05-20 15:06:00 +0900

    15.05

--- sbApi/src/main/java/com/web/app/mapper/InsMediationsDataMapper.java
+++ sbApi/src/main/java/com/web/app/mapper/InsMediationsDataMapper.java
@@ -1,7 +1,6 @@
 package com.web.app.mapper;
 
 import org.apache.ibatis.annotations.Mapper;
-
 import com.web.app.domain.Entity.CaseFileRelations;
 import com.web.app.domain.Entity.CaseMediations;
 import com.web.app.domain.Entity.Files;
@@ -19,7 +18,10 @@ public interface InsMediationsDataMapper {
 
     // 「調停案」にデータが存在するかどうかを判断する
     int mediationData(InsMediationsData mediationcase);
-    
+
+    // 「案件-添付ファイル」、「添付ファイル」データが存在するかどうかを判断する
+    int dataSearch(InsMediationsData mediationcase);
+
     // 「調停案」新規登録
     int insMediationsData2(CaseMediations CaseMediations);
 
@@ -28,10 +30,5 @@ public interface InsMediationsDataMapper {
 
     // 「案件-添付ファイルリレーション」新規登録
     int insCaseFileRelations(CaseFileRelations caseFileRelations);
-
-
-
-    // 「案件-添付ファイル」、「添付ファイル」データが存在するかどうかを判断する
-    int dataSearch(InsMediationsData mediationcase);
 
 }
