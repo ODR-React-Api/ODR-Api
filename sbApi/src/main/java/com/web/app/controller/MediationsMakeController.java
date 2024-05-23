8250e3bf9e0c0c72839ef347e34c281273a34333
authorxyr <xu.yiran@trans-cosmos.com.cn>
2024-05-23 16:34:00 +0900

    20240523

--- sbApi/src/main/java/com/web/app/controller/MediationsMakeController.java
+++ sbApi/src/main/java/com/web/app/controller/MediationsMakeController.java
@@ -4,6 +4,7 @@ package com.web.app.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import com.web.app.domain.Response;
@@ -12,7 +13,6 @@ import com.web.app.service.MediationsMakeService;
 import com.web.app.tool.AjaxResult;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
-import io.swagger.v3.oas.annotations.parameters.RequestBody;
 
 
 /**
