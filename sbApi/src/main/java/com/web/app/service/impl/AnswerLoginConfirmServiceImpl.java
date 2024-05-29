311b9526043b935463e912f8f188418711ab2cb8
yang.beibei@trans-cosmos.com.cn <yang.beibei@trans-cosmos.com.cn>
2024-05-29 11:57:22 +0900

    0529-update

--- sbApi/src/main/java/com/web/app/service/impl/AnswerLoginConfirmServiceImpl.java
+++ sbApi/src/main/java/com/web/app/service/impl/AnswerLoginConfirmServiceImpl.java
@@ -1,6 +1,5 @@
 package com.web.app.service.impl;
 
-import java.util.ArrayList;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
